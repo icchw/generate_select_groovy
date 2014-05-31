9477a110a5e8952ba3a2bec7b98e6583ea4f937c
icchw <kh.wada@gmail.com>
2014-06-01 02:03:42 +0900

    completed

--- generate_select.groovy
+++ generate_select.groovy
@@ -13,6 +13,8 @@
 
 import org.apache.poi.ss.usermodel.*
 import org.jggug.kobo.gexcelapi.*
+import Column
+
 
 // expand Cell class
 Cell.metaClass.define {
@@ -27,45 +29,68 @@ Cell.metaClass.define {
   }
 }
 
-def book = GExcel.open('table.xlsx')
-def sheet = book[0]
-def from = [args[0], '']
-def joins = [] // [tablePName, tableLName, ons = [joinTable, columnPName, columnLName]]
-def selects = []
-sheet.eachWithIndex {row, i ->
-  if (i <= 1) {
-    // skip header row
-    return
+// extract columns info from excel file
+def getVal(cell) {
+  if (cell == null) {
+    return ''
   }
-  def tablePName = row[0].evaluatedValue
-  def tableLName = row[1].evaluatedValue
-  def columnPName = row[2].evaluatedValue
-  if (!args.contains(tablePName)) {
-    return 
+  return cell.evaluatedValue
+}
+def extractColumns(fileName) {
+  def columns = []
+  def book = GExcel.open(fileName)
+  def sheet = book[0]
+  sheet.eachWithIndex {row, i ->
+    if (i <= 1) {
+      // skip header row
+      return
+    }
+    def tablePName = row[0].evaluatedValue
+    if (!args.contains(tablePName)) {
+      return 
+    }
+    columns << new Column(getVal(row[0]), getVal(row[1]), getVal(row[2]), getVal(row[3]))
   }
-  if (from[1].empty && !tableLName.empty && from[0] == tablePName) {
-    from[1] = tableLName
-  }
-  if (from[0] != tablePName && !joins.collect{it[0]}.contains(tablePName)) {
-    joins << [tablePName, tableLName, []]
-  }
-  if (selects.empty || !selects.collect{it[2]}.contains(columnPName)) {
-    selects << row.collect{it.evaluatedValue}
-  } else {
-    def joinTable = selects.find{it[2] == columnPName}[0]
-    def ons = joins.find{it[0] == tablePName}[2]
-    ons << [joinTable, columnPName, row[3].evaluatedValue]
-  }
+  return columns
 }
 
+def columns = extractColumns('table.xlsx')
+def selects = []          // [tablePName, tableLName, columnPName, columnLName]
+def from = [args[0], '']  // [tablePName. tableLName]
+def joins = []            // [tablePName, tableLName, ons = [joinTable, columnPName, columnLName]]
+args.eachWithIndex {tablePName, i -> 
+  columns.each {c ->
+    if (c.tablePName != tablePName) {
+      return
+    }
+    if (i == 0 && from[1].empty && !c.tableLName.empty) {
+      from[1] = c.tableLName
+    }
+    if (i != 0) {
+      def joinTable = joins.find{it[0] == tablePName}
+      if (joinTable == null) {
+        joinTable = [c.tablePName, '', []]
+        joins << joinTable
+      }
+      if (joinTable[1].empty && !c.tableLName.empty) {
+        joinTable[1] = c.tableLName
+      }
+    }
+    if (selects.empty || !selects.collect{it.columnPName}.contains(c.columnPName)) {
+      selects << c
+    } else {
+      def joinTable = selects.find{it.columnPName == c.columnPName}.tablePName
+      def ons = joins.find{it[0] == tablePName}[2]
+      ons << [joinTable, c.columnPName, c.columnLName]
+    }
+  }
+}
 def templateFileName = 'template.sql'
 def f = new File(templateFileName)
 def engine = new groovy.text.SimpleTemplateEngine()
 def binding = ['from' : from, 'selects' :  selects, 'joins' : joins ]
 def template = engine.createTemplate(f).make(binding)
-
 def date = new Date().format('yyyyMMdd_HHmmss')
 def resultFileName = date + '.sql'
-// def result = new File(resultFileName)
-// result.write(template.toString(), 'SJIS')
-println template.toString()
+def result = new File(resultFileName)
+result.write(template.toString(), 'SJIS')
--- template.sql
+++ template.sql
@@ -1,16 +1,16 @@
 select
 <% def comment(value, space = 2) {
-     return value.empty ? '' : ' ' * space + '-- ' + value
+     return value == null || value.empty ? '' : ' ' * space + '-- ' + value
    }
-
+  
    binding['selects'].eachWithIndex{s, i -> 
 %>\
-  <%= i == 0 ? '' : ', ' %><%= s[0] %>.<%= s[2] %><%= comment(s[3], (i == 0 ? 4 : 2)) %>
+  <%= i == 0 ? '' : ', ' %><%= s.tablePName %>.<%= s.columnPName %><%= comment(s.columnLName, (i == 0 ? 4 : 2)) %>
 <% } %>\
 from <%= binding['from'][0] %><%= comment(binding['from'][1]) %>
 <% binding['joins'].each{ %>\
 left outer join <%= it[0] %><%= comment(it[1]) %>
-  <% it[2].eachWithIndex{on, j -> %>\
-  <%= j == 0 ? 'on  ' : 'and ' %><%= it[0] + '.' + on[1] %> = <%= on[0] +'.' + on[1] %><%= comment(on[2]) %>
-  <% } %>\
+<% it[2].eachWithIndex{on, j -> %>\
+  <%= j == 0 ? 'on  ' : 'and ' %><%= on[0] + '.' + on[1] %> = <%= it[0] +'.' + on[1] %><%= comment(on[2]) %>
+<% } %>\
 <% } %>\
